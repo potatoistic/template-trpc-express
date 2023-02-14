0548f83820d57e52ee5197ad345a9a02dc1141d2
Dhruv Saxena <saxenadhruv1927@gmail.com>
2023-02-14 17:13:42 +0000

    feat: added prehook commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
 export const sum = (numOne: number, numTwo: number): number => {
-  for (let i = 0; i < 10; i--) {
-    console.log('hello world')
-  }
-
-  const json = {
-    res: 1,
-    re: 2
-  }
-
   return numOne + numTwo
 }
